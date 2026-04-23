6f36b5ffe80bbc66d7560799d27e7ff96d2b9c6f
Adam Rebolledo <adam.rebolledo-ext@veolia.com>
2026-04-22 21:59:16 -0400

    cambios

--- turno/src/App.jsx
+++ turno/src/App.jsx
@@ -6,15 +6,30 @@ function App() {
   const [motive, setMotive] = useState('')
   const [date, setDate] = useState('')
   const [owner, setOwner] = useState('')
-  const [error, setError] = useState('')
+  const [error, setError] = useState({
+    pet: '',
+    vet: '',
+    motive: '',
+    date: '',
+    owner: '',
+  })
 
   const [turnos, setTurnos] = useState([])
 
   const agregarTurno = e => {
     e.preventDefault()
 
-    if (pet === '' || vet === '' || owner === '' || motive === '') {
-      setError('Campo Obligatorio')
+    const nuevoError = {
+      pet: '',
+      vet: '',
+      motive: '',
+      date: '',
+      owner: ''
+    }
+
+    if (pet === '') {
+      nuevoError.pet = 'Campo Obligatorio'
+      setError(nuevoError)
       return
     }
 
@@ -34,14 +49,13 @@ function App() {
       <form onSubmit={agregarTurno}>
         <button type="submit">Agregar</button>
         <input type="text" placeholder="Pet Name" value={pet} onChange={e => setPet(e.target.value)} />
+        {error.pet && <p style={{ color: 'red' }}>{error.pet}</p>}
         <input type="text" placeholder="Vet" value={vet} onChange={e => setVet(e.target.value)} />
         <input type="text" placeholder="Motive" value={motive} onChange={e => setMotive(e.target.value)} />
         <input type="date" value={date} onChange={e => setDate(e.target.value)} />
         <input type="text" placeholder="Owner" value={owner} onChange={e => setOwner(e.target.value)} />
       </form>
-
-
-
+        
           
 
       {turnos.map((turno, index) => (
